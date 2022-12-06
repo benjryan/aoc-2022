706e9302ecfa584112dfe366f0f3271941ab8340
Ben Ryan <benjryan15@gmail.com>
2022-12-06 16:55:32 +1100

    Day 6 fixes

--- day6/part2/main.c
+++ day6/part2/main.c
@@ -6,23 +6,20 @@ int main()
     
     char buffer[4096];
     int counter = 0;
+    int start_marker = 0;
     while ((buffer[counter++] = fgetc(file)) != EOF)
     {
         char c = buffer[counter - 1];
-
-        // ensure this new char is different to the previous ones
-        for (int i = 0; i < counter - 1; ++i)
+        for (int i = start_marker; i < counter - 1; ++i)
         {
             if (buffer[i] == c)
             {
-                // reset
-                buffer[0] = c;
-                counter = 1;
+                start_marker = i + 1;
                 break;
             }
         }
 
-        if (counter == 14)
+        if ((counter - start_marker) == 14)
         {
             printf("Answer: %lld\n", ftell(file));
             return 0;
